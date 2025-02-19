8c882cfd8dfd046dd573a6c7c32f95f472d5f19b
Caio Torres <torrescaio12@gmail.com>
2025-02-19 11:38:48 -0300

    feat: restaurant menu, url slug, url consumptionMethod

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,31 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  return (
+    <h1>
+      {slug}{consumptionMethod}
+    </h1>
+  );
+};
+
+export default RestaurantMenuPage;
